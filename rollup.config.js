312619461425dbd58ab218fbfc5dfb01845f9d11
Ilya <sidorenko@telros.ru>
2023-07-21 11:03:40 +0300

    added minify config

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from "rollup-plugin-node-resolve";
+import terser from '@rollup/plugin-terser';
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
 import styles from "rollup-plugin-styles";
@@ -22,13 +23,38 @@ export default [
         ],
     },
     {
+        input: "src/index.js", // your entry point
+        output: {
+            name: "SPB", // package name
+            file: pkg.iifeMin,
+            format: "iife",
+        },
+        plugins: [
+            styles(),
+            resolve(),
+            commonjs(),
+            babel({
+                exclude: ["node_modules/**"],
+            }),
+            terser(),
+        ],
+    },
+    {
         input: "src/index.js",
         output: [{ file: pkg.module, format: "es" }],
+        plugins: [
+            styles(),
+        ],
+    },
+    {
+        input: "src/index.js",
+        output: [{ file: pkg.main, format: "es" }],
         plugins: [
             styles(),
             babel({
                 exclude: ["node_modules/**"],
             }),
+            terser(),
         ],
     },
 ];
